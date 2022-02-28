2855ceebfa7fde7d04041eefef20ade9e5b6b593
brianberzins <brianberzins@gmail.com>
2022-02-28 13:42:19 -0600

    f - try to call

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "1.0"
 
     def betRequest(self, game_state):
-        return 100
+        return game_state['current_buy_in'] - game_state['players'][game_state["in_action"]]["bet"]
 
     def showdown(self, game_state):
         pass
---
+++ test_player.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+
+
+class TestPlayer(TestCase):
+    def test_bet_request(self):
+        self.fail()
