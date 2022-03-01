b91a924cd610edb719563ae0ba10fe3523debff8
brianberzins <brianberzins@gmail.com>
2022-03-01 13:33:14 -0600

    f - logging

--- player.py
+++ player.py
@@ -3,8 +3,24 @@ class Player:
     VERSION = "1.1"
 
     def betRequest(self, game_state):
+
+
+        # first orbit
+            # suited
+            # paired
+            # connectors
+            # face card/ace
+        print("=================================")
+        print(game_state)
+        print("orbits    %d" % game_state['orbits'])
+        print("round     {:d}".format(game_state['round']))
+        print("bet_index %d" % game_state['bet_index'])
         return game_state['current_buy_in'] - game_state['players'][game_state["in_action"]]["bet"]
 
     def showdown(self, game_state):
         pass
+
+    # TODO: is "orbit" correct
+    # def is_first_orbit(self, game_state):
+    #     return game_state['orbits'] == 0
 
