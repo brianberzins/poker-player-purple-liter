e1be7f4195452a2a83601b66888643053b57f06a
brianberzins <brianberzins@gmail.com>
2022-03-02 14:04:03 -0600

    logging

--- player.py
+++ player.py
@@ -3,8 +3,6 @@ class Player:
     VERSION = "3.3"
 
     def betRequest(self, game_state):
-
-
         # first orbit -> not valid
             # suited
             # paired
@@ -15,8 +13,20 @@ class Player:
         print("orbits    %d" % game_state['orbits'])
         print("round     {:d}".format(game_state['round']))
         print("bet_index %d" % game_state['bet_index'])
+
+        print("hole cards:")
+        print("    %s" % self.my_cards(game_state)[0])
+        print("    %s" % self.my_cards(game_state)[0])
+
+        print("community cards:")
+        for card in self.community_cards(game_state):
+            print("    %s" % card)
+
         if self.should_fold(game_state):
+            print("folding")
             return 0
+
+        print("calling")
         return game_state['current_buy_in'] - game_state['players'][game_state["in_action"]]["bet"]
 
     def showdown(self, game_state):
@@ -36,10 +46,18 @@ class Player:
     def my_cards(self, game_state):
         return game_state['players'][game_state["in_action"]]["hole_cards"]
 
-    # TODO: is "orbit" correct
-    # def is_first_orbit(self, game_state):
-    #     return game_state['orbits'] == 0
+    def community_cards(self, game_state):
+        return game_state['community_cards']
+
     def should_fold(self, game_state):
         cards = self.my_cards(game_state)
         return not (self.has_face_card(cards) or self.has_pair_hole_cards(cards))
+
+    def get_cards(self, game_state):
+        cards = self.my_cards(game_state)
+        cards.extend(self.community_cards(game_state))
+        return cards
+
+    # def should_raise(self, cards):
+    #     map(lambda c: c[], cards)
 
--- test_player.py
+++ test_player.py
@@ -1,48 +1,58 @@
-import player
+import approvaltests
+from player import Player
 import json
 
 # def test_bet_before():
-#     subject = player.Player()
+#     subject = Player()
 #     game_stats = json.load(open('example_should_fold.json'))
 #     assert subject.has_bet_before(game_stats)
 
 # def test_first_round_true():
-#     subject = player.Player()
+#     subject = Player()
 #     game_stats = json.load(open('example_should_fold.json'))
 #     assert subject.is_first_orbit(game_stats)
 #
 # def test_first_orbit_false():
-#     subject = player.Player()
+#     subject = Player()
 #     game_stats = json.load(open('example_call.json'))
 #     assert not subject.is_first_orbit(game_stats)
 
 def test_should_fold():
-    subject = player.Player()
+    subject = Player()
     game_state = json.load(open('example_should_fold.json'))
     assert subject.should_fold(game_state)
 
 def test_dont_fold():
-    subject = player.Player()
+    subject = Player()
     game_state = json.load(open('example_call.json'))
     assert not subject.should_fold(game_state)
 
 def test_not_fold_because_pairs():
-    subject = player.Player()
+    subject = Player()
     game_state = json.load(open('example_with_pair.json'))
     assert not subject.should_fold(game_state)
 
 def test_should_not_fold_because_face_cards():
-    subject = player.Player()
+    subject = Player()
     game_state = json.load(open('example_call.json'))
     assert not subject.should_fold(game_state)
 
+def test_get_all_cards():
+    subject = Player()
+    game_stats = json.load(open('example_call.json'))
+    approvaltests.verify_as_json(subject.get_cards(game_stats))
+
+# look at all cards for pairs/triples/straights/flush
+
+def test_should_raise():
+    assert Player().should_raise(json.load(open('example_set.json')))
 
 def test_call():
-    subject = player.Player()
+    subject = Player()
     game_stats = json.load(open('example_call.json'))
     assert 240 == subject.betRequest(game_stats)
 
 # def test_bet_first_round():
-#     subject = player.Player()
+#     subject = Player()
 #     game_stats = json.load(open('example_should_fold.json'))
 #     assert 480 == subject.betRequest(game_stats)
